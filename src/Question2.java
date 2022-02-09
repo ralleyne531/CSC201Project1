32e4f4d8e6e5e7d945e7de9e56b3adf007340c5d
ralleyne531 <ralleyne531@gmail.com>
2022-02-09 18:02:24 -0500

    Code That Checks For Running Time N = 3, Running Time = 867

---
+++ src/Question2.java
@@ -0,0 +1,15 @@
+public class Question2 {
+    public static void main(String[] args) {
+        long StartTime = System.nanoTime();
+        int n = 3;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum = sum + i;
+        }
+        long EndTime = System.nanoTime();
+        Long RT = EndTime - StartTime;
+        System.out.println(RT);
+
+
+    }
+}
