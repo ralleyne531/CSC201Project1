1bdf7b79ee2950e0d0f9a693fe1863f9555d0a91
ralleyne531 <ralleyne531@gmail.com>
2022-02-09 18:15:32 -0500

    Code That Checks For Running Time N = 5, Running Time = 1148

--- src/Question2Snippet2.java
+++ src/Question2Snippet2.java
@@ -1,7 +1,7 @@
 public class Question2Snippet2 {
     public static void main(String[] args){
         long StartTime = System.nanoTime();
-        int n = 3;
+        int n = 5;
         int sum = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i*i; j++) {
