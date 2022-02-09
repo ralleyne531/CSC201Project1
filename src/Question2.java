9b7189faa698919b68f837275fe734dff1cd1a72
ralleyne531 <ralleyne531@gmail.com>
2022-02-09 18:16:29 -0500

    Code That Checks For Running Time N = 100, Running Time = 3251

--- src/Question2.java
+++ src/Question2.java
@@ -1,7 +1,7 @@
 public class Question2 {
     public static void main(String[] args) {
         long StartTime = System.nanoTime();
-        int n = 10;
+        int n = 100;
         int sum = 0;
         for(int i = 0; i < n; i++){
             sum = sum + i;
